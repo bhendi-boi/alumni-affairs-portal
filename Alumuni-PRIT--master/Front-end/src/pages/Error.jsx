66f98e4fd00b6f1c31f0d83d1422eb339445708d
bhendi-boi <bhendiisgreen@gmail.com>
2022-10-31 15:51:24 +0530

    error page responsive

--- Alumuni-PRIT--master/Front-end/src/pages/Error.jsx
+++ Alumuni-PRIT--master/Front-end/src/pages/Error.jsx
@@ -3,20 +3,17 @@ import errorSvg from "../assets/pageNotFound.svg";
 
 const Error = () => {
   return (
-    <main className="h-[calc(100vh - 1rem)]">
-      <div className="h-[80%] w-full flex justify-center py-10">
-        <object data={errorSvg} alt="" className="self-center" height="350vh" />
+    <main className="max-h-[calc(100vh-4rem)] bg-base-content text-base-100 relative">
+      <div className="flex justify-center items-center px-10 pt-10">
+        <img src={errorSvg} alt="404 error" className="sm:w-[50%]" />
       </div>
-      <div className="text-xl text-center">
-        <p>
-          The webpage you requested is currently down or don't exist. Try
-          checking the URL for possible errors
-        </p>
+      <div className="text-2xl text-center py-10 md:py-4">
         <p>
           Click{" "}
-          <Link to="/" className="hover:text-blue-600">
+          <Link to="/" className="text-sky-600 hover:text-blue-700">
             here
-          </Link>{" "}
+          </Link>
+          {"  "}
           to go to home page
         </p>
       </div>
