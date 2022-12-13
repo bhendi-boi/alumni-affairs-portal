811af54150c04057919b790c05537b44fd585682
bhendi-boi <bhendiisgreen@gmail.com>
2022-12-13 15:22:46 +0530

    login page error text position fixed

--- Alumuni-PRIT--master/Front-end/src/pages/Login.jsx
+++ Alumuni-PRIT--master/Front-end/src/pages/Login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
     email: null,
     password: "",
   });
-  const error = useLocalStorageState("loginError", "password donot match");
+  const [error, setError] = useLocalStorageState("loginError", "");
   const [isDisabled, setIsDisabled] = useState(false);
   const navigate = useNavigate();
 
@@ -66,94 +66,85 @@ const Login = () => {
   return (
     <>
       <main className="relative flex items-center justify-center w-full min-h-screen overflow-hidden bg-base-content">
-        {/* todo: find a good bg */}
-        {/* <div className="login-bg"></div> */}
-
-        <div className="w-full h-full md:w-[80%] md:h-[80%] sm:z-10 flex flex-col items-center justify-center text-white ">
-          <section className="h-full w-full sm:h-[90%] sm:w-[80%] flex flex-col items-center sm:flex-row  sm:shadow-xl sm:rounded-2xl border border-base-content sm:border-[#262c5e] sm:bg-[#262c5e]">
-            {/* image-container */}
-            <picture className="w-full h-full sm:w-[40%] flex items-center p-20 sm:border-r">
-              <img src={alumniLogo} alt="alumni-logo" className="block" />
-            </picture>
-            <div className="w-full sm:w-[60%] h-full">
-              <form
-                className="flex flex-col items-center justify-center w-full space-y-4"
-                onSubmit={(e) => handleSubmit(e)}
-              >
-                <p className="self-center pt-4 text-2xl md:pt-6 lg:pt-8 xs:text-3xl">
-                  Login in here
-                </p>
-                <div className="w-[85%]">
-                  <label htmlFor="email" className="block py-2 text-xl">
-                    Email or Username
+        <section className="h-full w-full sm:h-[90%] sm:w-[80%] flex flex-col items-center sm:flex-row  sm:shadow-xl sm:rounded-2xl border border-base-content text-white sm:border-[#262c5e] sm:bg-[#262c5e]">
+          {/* image-container */}
+          <picture className="w-full h-full sm:w-[40%] p-10 sm:p-4 sm:border-r">
+            <img src={alumniLogo} alt="alumni-logo" />
+          </picture>
+          <div className="w-full sm:w-[60%] h-full">
+            <form
+              className="flex flex-col items-center justify-center w-full space-y-4"
+              onSubmit={(e) => handleSubmit(e)}
+            >
+              <p className="self-center pt-4 text-2xl md:pt-6 lg:pt-8 xs:text-3xl">
+                Login in here
+              </p>
+              <div className="w-[85%]">
+                <label htmlFor="email" className="block py-2 text-xl">
+                  Email or Username
+                </label>
+                <input
+                  className={
+                    "w-full px-4 py-2 transition duration-500 bg-transparent border-b-2 border-stone-100 border-solid text-slate-50 sm:text-lg focus:outline-none hover:border-blue-600 focus:border-blue-600 invalid:border-red-600" +
+                    (!error ? "border-red-600" : "")
+                  }
+                  type="email"
+                  name="email"
+                  id="email"
+                  value={data.email}
+                  required
+                  autoFocus
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="w-[85%]  mb-4">
+                <div className="flex items-center justify-between w-full">
+                  <label htmlFor="pwd" className="block py-2 text-xl">
+                    Password
                   </label>
-                  <input
-                    className={
-                      "w-full px-4 py-2 transition duration-1000 bg-transparent border-b-2 border-slate-400 border-solid text-slate-200 focus:outline-none hover:border-blue-600 focus:border-blue-600 invalid:border-red-600" +
-                      (!error ? "border-red-600" : "")
-                    }
-                    type="email"
-                    name="email"
-                    id="email"
-                    value={data.email}
-                    required
-                    autoFocus
-                    onChange={handleChange}
-                  />
-                </div>
-                <div className="w-[85%]  mb-4">
-                  <div className="flex items-center justify-between w-full">
-                    <label htmlFor="pwd" className="block py-2 text-xl">
-                      Password
-                    </label>
-                    <button
-                      onClick={handleForgotPassword}
-                      className="p-2 transition-colors duration-200 sm:mr-2 hover:text-blue-600"
-                      type="reset"
-                    >
-                      <span className="text-sm">Forgot your password ?</span>
-                    </button>
-                  </div>
-                  <input
-                    className={
-                      "w-full px-4 py-2 transition duration-1000 bg-transparent border-b-2 border-slate-400 border-solid text-slate-200 focus:outline-none hover:border-blue-600 focus:border-blue-600 invalid:border-red-600 " +
-                      (!error ? "border-red-600" : "")
-                    }
-                    type="password"
-                    name="password"
-                    id="pwd"
-                    minLength={8}
-                    maxLength={20}
-                    value={data.password}
-                    required
-                    onChange={handleChange}
-                  />
-                </div>
-                {!error && (
-                  <div>
-                    <p className="text-error">{error}</p>
-                  </div>
-                )}
-                <div className="block w-[85%] pt-4">
-                  <SubmitButton
-                    isDisabled={isDisabled}
-                    className={"block w-full h-10"}
-                    text={"Login"}
-                  />
-                </div>
-                <p className="self-center py-4 text-base border-t sm:border-none sm:pt-3">
-                  Don't have an account ?{" "}
-                  <Link
-                    to="/signup"
-                    className="underline text-sky-500 hover:text-blue-600 focus:text-blue-600 hover:scale-105 focus:scale-105"
+                  <button
+                    onClick={handleForgotPassword}
+                    className="p-2 text-base-300 transition-colors duration-200 sm:mr-2 hover:text-white"
+                    type="reset"
                   >
-                    Sign up
-                  </Link>
-                </p>
-              </form>
-            </div>
-          </section>
-        </div>
+                    <span className="text-sm">Forgot your password ?</span>
+                  </button>
+                </div>
+                <input
+                  className={
+                    "w-full px-4 py-2 transition duration-500 bg-transparent border-b-2 border-stone-100 border-solid text-slate-50 sm:text-lg focus:outline-none hover:border-blue-600 focus:border-blue-600 invalid:border-red-600" +
+                    (!error ? "border-red-600" : "")
+                  }
+                  type="password"
+                  name="password"
+                  id="pwd"
+                  minLength={8}
+                  maxLength={20}
+                  value={data.password}
+                  required
+                  onChange={handleChange}
+                />
+              </div>
+              {error && (
+                <p className="text-red-600 capitalize w-full ml-16">{error}</p>
+              )}
+              <SubmitButton
+                isDisabled={isDisabled}
+                className={"w-3/4"}
+                text={"Login"}
+              />
+              <p className="self-center py-4 text-base border-t sm:border-none sm:pt-3">
+                Don't have an account ?{" "}
+                <Link
+                  to="/signup"
+                  className="hover:underline focus-within:underline text-sky-500"
+                >
+                  Sign up
+                </Link>
+              </p>
+            </form>
+          </div>
+        </section>
       </main>
       <OtherLinks />
     </>
